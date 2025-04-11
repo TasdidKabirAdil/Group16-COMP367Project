a86a5589b8c9e9a024e45fd3c7e7ff089f5aef54
TasdidKabirAdil <tadil@my.centennialcollege.ca>
2025-04-11 14:24:13 -0400

    Initial Commit

---
+++ Group16-COMP367Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Group16_COMP367Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
